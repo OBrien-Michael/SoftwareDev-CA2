4dbd49271f3831af3283e774b997f762a00cf533
Michael O Brien <obrien.michael@outlook.ie>
2023-12-03 22:40:16 +0000

    Added unit tests for AudioBook and Book

--- src/test/java/library/AudioBookTest.java
+++ src/test/java/library/AudioBookTest.java
@@ -1,69 +1,198 @@
 package library;
 
+import library.exceptions.LibraryItemException;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AudioBookTest {
 
+    private AudioBook newAudioBookOne() throws LibraryItemException {
+
+        return new AudioBook(1,"TestAudioBook",1,true,"1234567891012");
+
+    }
+
+    private AudioBook newAudioBookTwo() throws LibraryItemException {
+
+        return new AudioBook(3,"TestAudioBookTwo",2,true,"9234567891012");
+
+    }
+
+
+
     @Test
     void getIsbn() {
+        try {
+            assertEquals("1234567891012",newAudioBookOne().getIsbn());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setIsbn() {
+        try {
+
+            AudioBook audioBookTest = newAudioBookOne();
+
+            audioBookTest.setIsbn("1111111111111");
+
+            assertEquals("1111111111111",audioBookTest.getIsbn());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void testToString() {
+        try {
+            AudioBook audioBookTest = newAudioBookOne();
+
+            String expectedToString = "AudioBook: Library Item ID = 1, Title=TestAudioBook, authorId=1, availability=true isbn=1234567891012\n";
+
+            assertEquals(expectedToString,audioBookTest.toString());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getLibraryItemId() {
+        try {
+            assertEquals(1,newAudioBookOne().getLibraryItemId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setLibraryItemId() {
+        try {
+            AudioBook audioBookTest = newAudioBookOne();
+
+            audioBookTest.setLibraryItemId(2);
+
+            assertEquals(2,audioBookTest.getLibraryItemId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getTitle() {
+        try {
+            assertEquals("TestAudioBook",newAudioBookOne().getTitle());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setTitle() {
+        try {
+            AudioBook audioBookTest = newAudioBookOne();
+
+            audioBookTest.setTitle("Learning Java");
+
+            assertEquals("Learning Java",audioBookTest.getTitle());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getAuthorId() {
+        try {
+            assertEquals(1,newAudioBookOne().getAuthorId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setAuthorId() {
+        try {
+            AudioBook audioBookTest = newAudioBookOne();
+
+            audioBookTest.setAuthorId(2);
+
+            assertEquals(2,audioBookTest.getAuthorId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getAvailability() {
+        try {
+            assertTrue(newAudioBookOne().getAvailability());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setAvailability() {
+        try {
+            AudioBook audioBookTest = newAudioBookOne();
+
+            audioBookTest.setAvailability(false);
+
+            assertFalse(audioBookTest.getAvailability());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
 
     @Test
     void compareTo() {
-    }
+        try {
+            AudioBook audioBookTestOne = newAudioBookOne();
+            AudioBook audioBookTestTwo = newAudioBookTwo();
 
-    @Test
-    void displayAllDetails() {
-    }
+            assertEquals(-1,audioBookTestOne.compareTo(audioBookTestTwo));
 
-    @Test
-    void displaySummaryDetails() {
+
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void convertToCommaDelimitedArray() {
+        try {
+            AudioBook audioBookTest = newAudioBookOne();
+
+            ArrayList<String> expectedToStringArray = new ArrayList<>();
+            expectedToStringArray.add("1");
+            expectedToStringArray.add("TestAudioBook");
+            expectedToStringArray.add("1");
+            expectedToStringArray.add("1234567891012");
+            expectedToStringArray.add("true");
+
+
+            assertEquals(expectedToStringArray,audioBookTest.convertToCommaDelimitedArray());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
+
+
     }
 }
--- src/test/java/library/BookTest.java
+++ src/test/java/library/BookTest.java
@@ -1,72 +1,197 @@
 package library;
 
+import library.exceptions.LibraryItemException;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
+    private Book newBookOne() throws LibraryItemException {
+
+        return new Book(1,"TestBook",1,true,"1234567891012");
+
+    }
+
+    private Book newBookTwo() throws LibraryItemException {
+
+        return new Book(3,"TestBookTwo",2,true,"9234567891012");
+
+    }
+
+
+
     @Test
     void getIsbn() {
+        try {
+            assertEquals("1234567891012",newBookOne().getIsbn());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setIsbn() {
+        try {
+            Book bookTest = newBookOne();
+
+            bookTest.setIsbn("1111111111111");
+
+            assertEquals("1111111111111",bookTest.getIsbn());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void testToString() {
+        try {
+            Book bookTest = newBookOne();
+
+            String expectedToString = "Library Item ID = 1, Title=TestBook, authorId=1, availability=true isbn=1234567891012";
+
+            assertEquals(expectedToString,bookTest.toString());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getLibraryItemId() {
+        try {
+            assertEquals(1,newBookOne().getLibraryItemId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setLibraryItemId() {
+        try {
+            Book bookTest = newBookOne();
+
+            bookTest.setLibraryItemId(2);
+
+            assertEquals(2,bookTest.getLibraryItemId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getTitle() {
+        try {
+            assertEquals("TestBook",newBookOne().getTitle());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setTitle() {
+        try {
+            Book bookTest = newBookOne();
+
+            bookTest.setTitle("Learning Java");
+
+            assertEquals("Learning Java",bookTest.getTitle());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getAuthorId() {
+        try {
+            assertEquals(1,newBookOne().getAuthorId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setAuthorId() {
+        try {
+            Book bookTest = newBookOne();
+
+            bookTest.setAuthorId(2);
+
+            assertEquals(2,bookTest.getAuthorId());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void getAvailability() {
+        try {
+            assertTrue(newBookOne().getAvailability());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void setAvailability() {
+        try {
+            Book bookTest = newBookOne();
+
+            bookTest.setAvailability(false);
+
+            assertFalse(bookTest.getAvailability());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
-    @Test
-    void testToString1() {
-    }
 
     @Test
     void compareTo() {
-    }
+        try {
+            Book bookTestOne = newBookOne();
+            Book bookTestTwo = newBookTwo();
 
-    @Test
-    void displayAllDetails() {
-    }
+            assertEquals(-1,bookTestOne.compareTo(bookTestTwo));
 
-    @Test
-    void displaySummaryDetails() {
+
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
     }
 
     @Test
     void convertToCommaDelimitedArray() {
+        try {
+            Book bookTest = newBookOne();
+
+            ArrayList<String> expectedToStringArray = new ArrayList<>();
+            expectedToStringArray.add("1");
+            expectedToStringArray.add("TestBook");
+            expectedToStringArray.add("1");
+            expectedToStringArray.add("1234567891012");
+            expectedToStringArray.add("true");
+
+
+            assertEquals(expectedToStringArray,bookTest.convertToCommaDelimitedArray());
+        } catch (LibraryItemException libraryItemException) {
+            fail();
+            System.out.println(libraryItemException.getLibraryItemExceptionMessage());
+        }
+
+
     }
 }
