7eeeccbaa5b23ac15000bf49813856634de0b057
Marius <clous.sor44@gmail.com>
2023-12-03 15:41:33 +0000

    sda

--- src/main/java/library/DVD.java
+++ src/main/java/library/DVD.java
@@ -1,40 +1,46 @@
 package library;
-
+// Importing necessary classes and abstract classes
 import library.abstracts.LibraryItem;
 import library.abstracts.MediaItem;
 import library.exceptions.LibraryItemException;
 
 import java.util.ArrayList;
+   //The DVD class extends MediaItem and represents a DVD in the library system.
 
 public class DVD extends MediaItem {
 
     public DVD() {
     }
-
+    // Parameterized constructor for creating a DVD with specified details
     public DVD(int libraryItemId, String title, int authorId, boolean availability, int playtime) throws LibraryItemException {
         super(libraryItemId, title, authorId, availability, playtime);
     }
 
+    // Overridden toString method for DVD class
     @Override
     public String toString() {
         return "DVD: " + super.toString()+"\n";
     }
 
+    // Overridden compareTo method for comparing DVDs by title
     @Override
     public int compareTo(LibraryItem libraryItem) {
         return this.getTitle().compareTo(libraryItem.getTitle());
     }
 
+    // Overridden displayAllDetails method to display all details of the DVD
     @Override
     public void displayAllDetails() {
         System.out.println("DVD: Item ID: "+this.getLibraryItemId()+", Title: "+this.getTitle()+" Author ID: "+this.getAuthorId()+", Playtime: "+this.getPlaytime()+", Available: "+this.getAvailability());
     }
 
+    // Overridden displaySummaryDetails method to display summary details of the DVD
     @Override
     public void displaySummaryDetails() {
         System.out.println("DVD: Item ID: "+this.getLibraryItemId()+", Title: "+this.getTitle()+", Available: "+this.getAvailability());
     }
 
+    // Overridden convertToCommaDelimitedArray method to convert DVD details to CSV format
     @Override
     public ArrayList<String> convertToCommaDelimitedArray() {
         ArrayList<String> csvRecord = new ArrayList<>();
--- src/main/java/library/Dissertation.java
+++ src/main/java/library/Dissertation.java
@@ -1,42 +1,47 @@
 package library;
-
+// Importing necessary classes and packages
 import library.abstracts.LibraryItem;
 import library.abstracts.ResearchItem;
 import library.exceptions.LibraryItemException;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+// Dissertation class extends ResearchItem to represent dissertations in the library system
 public class Dissertation extends ResearchItem {
 
     public Dissertation() {
     }
-
+    // Parameterized constructor for creating a Dissertation with all properties
     public Dissertation(int libraryItemId, String title, int authorId, boolean availability, String topic, String abstractText, LocalDate datePublished) throws LibraryItemException {
         super(libraryItemId, title, authorId, availability, topic, abstractText, datePublished);
     }
 
+    // Returns a string representation of the Dissertation
     @Override
     public String toString() {
         return "Dissertation: " + super.toString()+"\n";
     }
 
+    // Compares this Dissertation to another LibraryItem by title
     @Override
     public int compareTo(LibraryItem libraryItem) {
         return this.getTitle().compareTo(libraryItem.getTitle());
     }
 
+    // Prints all details of the Dissertation
     @Override
     public void displayAllDetails() {
         System.out.println("Dissertation: Item ID: "+this.getLibraryItemId()+", Title: "+this.getTitle()+" Author ID: "+this.getAuthorId()+", " +
                 "Available: "+this.getAvailability()+", Topic: "+this.getTopic()+", Abstract: "+this.getAbstractText()+", Date Published: "+this.getDatePublished());
     }
-
+    // Prints a summary of the Dissertation's details
     @Override
     public void displaySummaryDetails() {
         System.out.println("Dissertation: Item ID: "+this.getLibraryItemId()+", Title: "+this.getTitle()+", Available: "+this.getAvailability());
     }
 
+
+    // Converts the Dissertation's details into a comma-delimited array for CSV format
     @Override
     public ArrayList<String> convertToCommaDelimitedArray() {
         ArrayList<String> csvRecord = new ArrayList<>();
